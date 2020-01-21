e0d89d4fc4d71c7d0beeeeae0323358af314ed56
Anto85 <40073356+Anto85@users.noreply.github.com>
2020-01-21 16:11:10 +0100

    Create panneau.py

---
+++ panneau.py
@@ -0,0 +1,20 @@
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread("difficult.PNG")
+img_gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+Filtre_Stop = cv2.CascadeClassifier("stopsign_classifier.xml")
+
+stop = Filtre_Stop.detectMultiScale(img_gris, scaleFactor=1.1, minNeighbors=4, minSize=(20, 20))
+
+print(stop)
+n = len(stop)
+
+if n != 0:
+    for (x, y, w, h) in stop:
+        cv2.rectangle(img_rgb, (x, y), (x+h, y+w), (0, 255, 0), 5)
+plt.subplot(1, 1, 1)
+plt.imshow(img_rgb)
+plt.show()
