25a3669e799f8704f605e0784e4f80846a00c404
Aubin Sionville <57773476+ASionville@users.noreply.github.com>
2020-02-05 18:35:44 +0100

    No args anymore

--- real_time_object_detection.py
+++ real_time_object_detection.py
@@ -14,22 +14,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def colorFader(mix=0): #fade (linear interpolate) from color c1 (at mix=0) to c2 (mix=1)
-    c1=np.array(mpl.colors.to_rgb('red'))
-    c2=np.array(mpl.colors.to_rgb('green'))
-    hexcolor = mpl.colors.to_hex((1-mix)*c1 + mix*c2)
+    red=np.array(mpl.colors.to_rgb('red'))
+    green=np.array(mpl.colors.to_rgb('green'))
+    hexcolor = mpl.colors.to_hex((1-mix)*red + mix*green)
     rgbcolor = mpl.colors.to_rgb(hexcolor)
     output = (rgbcolor[2]*255, rgbcolor[1]*255, rgbcolor[0]*255) #BGR
     return output
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.2,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
 
 # initialize the list of class labels MobileNet SSD was trained to
 # detect, then generate a set of bounding box colors for each class
@@ -43,62 +33,69 @@ FRENCH = ["Arriere plan", "Avion", "Velo", "Oiseau", "Bateau",
 	"Chien", "Cheval", "Moto", "Personne", "Plante", "Mouton", "Canape", "Train", "TV"]
 
 COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
 for color in COLORS:
 	color = color[::-2]
+
 print(COLORS[15])
 # load our serialized model from disk
-print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+print("[INFO] Chargement du modele...")
+
+prototxt = "MobileNetSSD_deploy.prototxt"
+model = "MobileNetSSD_deploy.caffemodel"
+
+infos = cv2.dnn.readNetFromCaffe(prototxt, model)
 
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
-print("[INFO] starting video stream...")
-vs = VideoStream(src=1).start()
+print("[INFO] Chargement video...")
+stream = VideoStream(src=0).start()
 time.sleep(2.0)
 fps = FPS().start()
 
 # loop over the frames from the video stream
 while True:
 	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=800)
+	frame = stream.read()
+	frame = imutils.resize(frame, width=1000)
 
 	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
+	(height, width) = frame.shape[:2]
 	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
 		0.007843, (300, 300), 127.5)
 
 	# pass the blob through the network and obtain the detections and
 	# predictions
-	net.setInput(blob)
-	detections = net.forward()
+	infos.setInput(blob)
+	detections = infos.forward()
 
 	# loop over the detections
 	for i in np.arange(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with
+		# extract the percent_sure (i.e., probability) associated with
 		# the prediction
-		confidence = detections[0, 0, i, 2]
+		percent_sure = detections[0, 0, i, 2]
 
-		# filter out weak detections by ensuring the `confidence` is
-		# greater than the minimum confidence
-		if confidence > args["confidence"]:
+		# filter out weak detections by ensuring the `percent_sure` is
+		# greater than the minimum percent_sure
+		if percent_sure > 0.4:
 			# extract the index of the class label from the
 			# `detections`, then compute the (x, y)-coordinates of
 			# the bounding box for the object
-			idx = int(detections[0, 0, i, 1])
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			objX = int(detections[0, 0, i, 1])
+			box = detections[0, 0, i, 3:7] * np.array([width, height, width, height])
 			(startX, startY, endX, endY) = box.astype("int")
 
 			# draw the prediction on the frame
-			label = "{}: {:.2f}%".format(FRENCH[idx],
-				round(confidence * 100, 2))
+			label = "{}: {:.2f}%".format(FRENCH[objX],
+				round(percent_sure * 100, 2))
+
 			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				colorFader(confidence), 2)
+				colorFader(percent_sure), 2)
+
 			y = startY - 15 if startY - 15 > 15 else startY + 15
 
 			cv2.putText(frame, label, (startX, y),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, colorFader(confidence), 2)
+				cv2.FONT_HERSHEY_SIMPLEX, 0.5, colorFader(percent_sure), 2)
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
@@ -113,9 +110,9 @@ while True:
 
 # stop the timer and display FPS information
 fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+print("[INFO] Delta time : {:.2f}".format(fps.elapsed()))
+print("[INFO] FPS :        {:.2f}".format(fps.fps()))
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
-vs.stop()
+stream.stop()
