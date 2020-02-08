5c3903c6a053aca5eb768ecb1ea7355f73f49567
Mathias <mathias.jomeau@gmail.com>
2020-02-08 07:44:36 +0100

    réorganisation fichier xml

--- organisation_file.py
+++ organisation_file.py
@@ -3,12 +3,11 @@ from PIL import Image
 from os import listdir
 from os.path import isfile, join
 
-
-def resize_rename_replace(mypath, new_path, new_name, extension):
+def resize_rename_replace(mypath, new_path, new_name, extension, file):
     imageFiles = [f for f in listdir(mypath) if isfile(join(mypath, f)) and (f.endswith("G") or f.endswith("g"))]
     #liste = os.listdir(mypath)
     target = 1000
-    count = 1
+    txt = ""
 
 
     for im in imageFiles:
@@ -23,32 +22,19 @@ def resize_rename_replace(mypath, new_path, new_name, extension):
             width = int(height * ratio)
 
         im2 = im1.resize((width, height), Image.ANTIALIAS)  # linear interpolation in a 2x2 environment
-        im2.save(join(new_path, (new_name + str(count) + '.' + extension)))
-        count += 1
+        im3 = im2.save(join(new_path, (new_name + str(count) + '.' + extension)))
+        txt = txt + "negatives/" + str(im3) + "\n"
+
         print(im, "redimensionnée…")
+
     print("Travail terminé !", len(imageFiles), "images redimensionnées.")
-
-
-def write_txt_images(file, path):
-    liste = os.listdir(path)
-    os.chdir(path)
-    txt = ""
-    for i in range(len(liste)):
-        name = liste[i]
-        txt = txt + str(name) + "\n"
     file.write(txt)
 
 
+now_path = "/media/mathias/MATHIAS/negatifs"
+new_path = "negatives"
 
-
-now_path_neg = "/home/mathias/Images/Images Stop Sign/Images Stop Sign/negatifs"
-new_path_neg = "/home/mathias/Dropbox/Informatique/Python/IA/Haar_Cascade/1- Panneau/Création xml/negatifs"
-
-file = 'negatifs.txt'
+file = 'negatives.txt'
 fichier = open(file, "w")
 
-#resize_rename_replace(now_path_neg, new_path_neg, 'image', "jpg")
-write_txt_images(fichier, new_path_neg)
-
-
-
+resize_rename_replace(now_path, new_path, 'image', "jpg", fichier)
