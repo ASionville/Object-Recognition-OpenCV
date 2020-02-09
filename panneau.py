b3b5f3f1400e4d4ae3ffe1ebf7e0cae6ec1b3d74
Mathias <mathias.jomeau@gmail.com>
2020-02-09 13:20:16 +0100

    edit panneau

--- panneau.py
+++ panneau.py
@@ -6,14 +6,14 @@ comptant des caractéristiques dans celui-ci un panneau stop.
 Ensuite le programme met on évidence les point communs
 qu'il a trouve par le biais d'un triangle vert
 """
-image = cv2.imread("image.png")  # On ouvre l'image
-image_gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # On met l'image en noir
+image = cv2.imread("XML/images_test/roadstop.jpg")  # On ouvre l'image
+image_gris = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # On met l'image en noir et blanc
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # On met l'image en couleur
 
 Filtre_Stop = cv2.CascadeClassifier(
-    "panneau.xml")  # On prends notre fichier xml qui nous permet de trouvé ce que l'on cherche dans l'image
+    "XML/data/cascade.xml")  # On prends notre fichier xml qui nous permet de trouvé ce que l'on cherche dans l'image
 
-stops = Filtre_Stop.detectMultiScale(image_gris, scaleFactor=1.1, minNeighbors=4,
+stops = Filtre_Stop.detectMultiScale(image_gris, scaleFactor=1.1, minNeighbors=50,
                                      minSize=(20, 20))  # On compare les deux images pour trouver des similitudes
 
 n = len(stops)
